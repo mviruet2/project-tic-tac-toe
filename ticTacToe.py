8268cb7456458baa525ff27eba1ab9f2858f6fa4
Meralys Viruet <mviruet2@mail.valenciacollege.edu>
2016-03-21 19:58:32 -0500

    Fixed the issue

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,6 +1,3 @@
-def printBoard(board):{'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-                    'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-                    'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 def printBoard(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
     print('-+-+-')
